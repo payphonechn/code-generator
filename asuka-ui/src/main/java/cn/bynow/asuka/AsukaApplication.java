b46b8edd367f6932616f4e3b6983a59bdfc815ae
Bynow(伯言) <1923184056@qq.com>
2020-05-30 16:56:03 +0800

    替换加载闪图

--- asuka-ui/src/main/java/cn/bynow/asuka/AsukaApplication.java
+++ asuka-ui/src/main/java/cn/bynow/asuka/AsukaApplication.java
@@ -1,11 +1,19 @@
 package cn.bynow.asuka;
 
 import de.felixroske.jfxsupport.AbstractJavaFxApplicationSupport;
+import javafx.stage.Stage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class AsukaApplication extends AbstractJavaFxApplicationSupport {
     public static void main(String[] args) {
-        launch(AsukaApplication.class, HelloworldView.class, args);
+        launch(AsukaApplication.class, HelloworldView.class,new SplashView(), args);
+    }
+
+    @Override
+    public void beforeInitialView(Stage stage, ConfigurableApplicationContext ctx) {
+        stage.setWidth(1000);
+        stage.setHeight(1000);
     }
 }
---
+++ asuka-ui/src/main/java/cn/bynow/asuka/SplashView.java
@@ -0,0 +1,28 @@
+package cn.bynow.asuka;
+
+import de.felixroske.jfxsupport.SplashScreen;
+import javafx.scene.Parent;
+
+/**
+ * @author Bynow
+ * @version 1.0
+ * @date 2020/5/30 16:33
+ */
+public class SplashView extends SplashScreen {
+
+    @Override
+    public Parent getParent() { // 在这里可以设置闪屏窗口大小，默认图片大小
+        return super.getParent();
+    }
+
+    @Override
+    public boolean visible() { // 是否显示闪屏，默认显示
+        return super.visible();
+    }
+
+    @Override
+    public String getImagePath() {
+        return "/static/asuka.gif";
+    }
+
+}
