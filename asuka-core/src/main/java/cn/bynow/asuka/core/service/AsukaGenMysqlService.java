a08b9f27e05419706114c3b4f27eceb80988e803
Bynow(伯言) <1923184056@qq.com>
2020-06-03 23:53:55 +0800

    添加代码生成器业务部分

--- asuka-core/src/main/java/cn/bynow/asuka/core/service/AsukaGenMysqlService.java
+++ asuka-core/src/main/java/cn/bynow/asuka/core/service/AsukaGenMysqlService.java
@@ -1,9 +1,13 @@
 package cn.bynow.asuka.core.service;
 
 import cn.bynow.asuka.core.utils.DataSourceUtil;
+import cn.bynow.asuka.core.utils.AsukaGenUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -15,15 +19,22 @@ import java.util.Map;
 public class AsukaGenMysqlService {
 
     private final String TABLE_NAME = "sys_user";
-    private final String URL = "jdbc:mysql://127.0.0.4:3306/guns";
+    private final String URL = "jdbc:mysql://127.0.0.1:3306/guns?useUnicode=true&amp;characterEncoding=UTF-8&amp;useInformationSchema=true&amp;useSSL=false";
     private final String DRIVER = "com.mysql.jdbc.Driver";
     private final String USER = "root";
-    private final String PASSWORD = "root!";
+    private final String PASSWORD = "root0301!";
 
-    public void test(){
+    public void test() throws Exception {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
         JdbcTemplate template = new JdbcTemplate(DataSourceUtil.getDataSource(DRIVER,URL,USER,PASSWORD));
-        Map list = template.queryForMap("select * from sys_user");
-        System.out.println(list);
+        //根据表名查询表实例
+        Map table = template.queryForMap("select table_name tableName, engine, table_comment tableComment, create_time createTime from information_schema.tables where table_schema = (select database()) and table_name = 'sys_user'");
+        List columns = template.queryForList("select column_name columnName, data_type dataType, column_comment columnComment, column_key columnKey, extra from information_schema.columns where table_name = 'sys_user' and table_schema = (select database()) order by ordinal_position");
+        AsukaGenUtils.generatorCode(table, columns);
+
+
+
     }
 
 
---
+++ asuka-core/src/main/java/cn/bynow/asuka/core/utils/AsukaGenUtils.java
@@ -0,0 +1,188 @@
+package cn.bynow.asuka.core.utils;
+
+import cn.bynow.asuka.core.entity.ColumnEntity;
+import cn.bynow.asuka.core.entity.TableEntity;
+import org.apache.commons.configuration.Configuration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.WordUtils;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+/**
+ * 代码生成器   工具类
+ *
+ * @author czx
+ * @email object_czx@163.com
+ * @date 2016年12月19日 下午11:40:24
+ */
+public class AsukaGenUtils {
+
+    public static List<String> getTemplates(){
+        List<String> templates = new ArrayList<String>();
+        templates.add("template/Entity.java.vm");
+        templates.add("template/Dao.java.vm");
+        templates.add("template/Dao.xml.vm");
+        templates.add("template/Service.java.vm");
+        templates.add("template/ServiceImpl.java.vm");
+        templates.add("template/Controller.java.vm");
+        return templates;
+    }
+
+    /**
+     * 生成代码
+     */
+    public static void generatorCode(Map<String, String> table,List<Map<String, String>> columns) throws Exception {
+        boolean hasBigDecimal = false;
+        //表信息
+        TableEntity tableEntity = new TableEntity();
+        tableEntity.setTableName(table.get("tableName" ));
+        tableEntity.setComments(table.get("tableComment" ));
+        //表名转换成Java类名
+        String className = columnToJava(tableEntity.getTableName());
+        tableEntity.setClassName(className);
+        tableEntity.setClassname(StringUtils.uncapitalize(className));
+
+        //列信息
+        List<ColumnEntity> columsList = new ArrayList<>();
+        for(Map<String, String> column : columns){
+            ColumnEntity columnEntity = new ColumnEntity();
+            columnEntity.setColumnName(column.get("columnName" ));
+            columnEntity.setDataType(column.get("dataType" ));
+            columnEntity.setComments(column.get("columnComment" ));
+            columnEntity.setExtra(column.get("extra" ));
+
+            //列名转换成Java属性名
+            String attrName = columnToJava(columnEntity.getColumnName());
+            columnEntity.setAttrName(attrName);
+            columnEntity.setAttrname(StringUtils.uncapitalize(attrName));
+
+            //列的数据类型，转换成Java类型
+            String attrType = columnEntity.getDataType();
+            columnEntity.setAttrType(attrType);
+            if (!hasBigDecimal && attrType.equals("BigDecimal" )) {
+                hasBigDecimal = true;
+            }
+            //是否主键
+            if ("PRI".equalsIgnoreCase(column.get("columnKey" )) && tableEntity.getPk() == null) {
+                tableEntity.setPk(columnEntity);
+            }
+
+            columsList.add(columnEntity);
+        }
+        tableEntity.setColumns(columsList);
+
+        //没主键，则第一个字段为主键
+        if (tableEntity.getPk() == null) {
+            tableEntity.setPk(tableEntity.getColumns().get(0));
+        }
+
+        //设置velocity资源加载器
+        Properties prop = new Properties();
+        prop.put("file.resource.loader.class", "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader" );
+        Velocity.init(prop);
+        String mainPath = "cn.bynow.asuka";
+        //封装模板数据
+        Map<String, Object> map = new HashMap<>();
+        map.put("tableName", tableEntity.getTableName());
+        map.put("comments", tableEntity.getComments());
+        map.put("pk", tableEntity.getPk());
+        map.put("className", tableEntity.getClassName());
+        map.put("classname", tableEntity.getClassname());
+        map.put("pathName", tableEntity.getClassname().toLowerCase());
+        map.put("columns", tableEntity.getColumns());
+        map.put("hasBigDecimal", hasBigDecimal);
+        map.put("mainPath", mainPath);
+        map.put("package", "cn.bynow.asuka");
+        map.put("moduleName", "asuka");
+        map.put("author", "asuka");
+        map.put("email", "test");
+        map.put("datetime", DateUtils.format(new Date(), DateUtils.DATE_TIME_PATTERN));
+        VelocityContext context = new VelocityContext(map);
+
+        //获取模板列表
+        List<String> templates = getTemplates();
+        for (String template : templates) {
+            try {
+            //渲染模板
+            Template tpl = Velocity.getTemplate(template, "UTF-8" );
+
+            File saveFile = new File("D:\\test\\"+getFileName(template, "","", "asuka"));
+            FileOutputStream outStream = new FileOutputStream(saveFile);
+            OutputStreamWriter writer = new OutputStreamWriter(outStream);
+            BufferedWriter bufferWriter = new BufferedWriter(writer);
+            tpl.merge(context, bufferWriter);
+            bufferWriter.flush();
+            outStream.close();
+            bufferWriter.close();
+
+
+            } catch (Exception e) {
+                throw new Exception("渲染模板失败，表名：" + tableEntity.getTableName(), e);
+            }
+        }
+    }
+
+
+    /**
+     * 列名转换成Java属性名
+     */
+    public static String columnToJava(String columnName) {
+        return WordUtils.capitalizeFully(columnName, new char[]{'_'}).replace("_", "" );
+    }
+
+    /**
+     * 获取配置信息
+     */
+    public static Configuration getConfig() throws Exception {
+        try {
+            return new PropertiesConfiguration("generator.properties" );
+        } catch (ConfigurationException e) {
+            throw new Exception("获取配置文件失败，", e);
+        }
+    }
+
+    /**
+     * 获取文件名
+     */
+    public static String getFileName(String template, String className, String packageName, String moduleName) {
+        String packagePath = "";
+        if (StringUtils.isNotBlank(packageName)) {
+            packagePath += packageName.replace(".", File.separator) + File.separator + moduleName + File.separator;
+        }
+
+        if (template.contains("Entity.java.vm" )) {
+            return File.separator + className + "Entity.java";
+        }
+
+        if (template.contains("Dao.java.vm" )) {
+            return File.separator + className + "Dao.java";
+        }
+
+        if (template.contains("Service.java.vm" )) {
+            return File.separator + className + "Service.java";
+        }
+
+        if (template.contains("ServiceImpl.java.vm" )) {
+            return File.separator + className + "ServiceImpl.java";
+        }
+
+        if (template.contains("Controller.java.vm" )) {
+            return   File.separator + className + "Controller.java";
+        }
+
+        if (template.contains("Dao.xml.vm" )) {
+            return   File.separator + className + "Dao.xml";
+        }
+
+        return null;
+    }
+}
